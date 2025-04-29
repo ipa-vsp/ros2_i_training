9561bd17aa27b8805f47acc8397984e3aeebc450
vsp-ar <aswathy.ramachandran.menon@ipa.fraunhofer.de>
2025-04-29 23:40:53 +0200

    added copy code option in conf.py and updated the urdf tutorial

--- workshop/source/conf.py
+++ workshop/source/conf.py
@@ -3,6 +3,7 @@
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+
 # -- Path setup --------------------------------------------------------------
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -16,48 +17,53 @@ from sphinx import version_info as sphinx_version_info
 project = 'ROS2 workshop'
 copyright = '2025, Fraunhofer IPA'
 author = 'Ragesh Ramachandran, Aswathy Ramachandran, Yasmine Makkaoui'
+
 # -- General configuration ---------------------------------------------------
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.githubpages',
+    'myst_parser',            # Enables Markdown support
+    'sphinx_copybutton',      # Adds "copy" buttons to code blocks
+]
 source_suffix = ['.rst', '.md']
 # The master toctree document.
 master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md', 'exercise']
-extensions = ['sphinx.ext.githubpages']
 
+# Handle different versions of Sphinx
+# if sphinx_version_info >= (1, 4):
+#     extensions += ['recommonmark']
+# else:
+#     from recommonmark.parser import CommonMarkParser
+#     source_parsers = {
+#         '.md': CommonMarkParser,
+#     }
 
-if sphinx_version_info >= (1, 4):
-  extensions += ['recommonmark']
-else:
-  from recommonmark.parser import CommonMarkParser
-
-  source_parsers = {
-      '.md': CommonMarkParser,
-  }
 # -- Options for HTML output -------------------------------------------------
-# The theme to use for HTML and HTML Help pages.  See the documentation for
+# The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Customize the theme options
 html_theme_options = {
-    #'canonical_url': '',
-    'analytics_id': '',
     'logo_only': False,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
-    #'vcs_pageview_mode': '',
-    # Toc options
     'collapse_navigation': True,
     'sticky_navigation': False,
     'navigation_depth': 4,
